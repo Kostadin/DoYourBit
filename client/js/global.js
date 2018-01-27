13b2141070ac2e5c86f22d43dce873efbaa14dd0
Nayden Pendov <darkey@protonmail.com>
2018-01-27 14:40:58 +0200

    client interface

--- client/js/global.js
+++ client/js/global.js
@@ -8,11 +8,11 @@ var TILE_WIDTH = 64;
 var HALF_TILE_HEIGHT = TILE_HEIGHT/2.0;
 var HALF_TILE_WIDTH = TILE_WIDTH/2.0;
 
-var player = null;
-var guards = null;
+//var player = null;
+//var guards = null;
 
-var playerAnimations;
-var guardAnimations;
+//var playerAnimations;
+//var guardAnimations;
 
 var gameStartTime = null;
 var currentTime = null;
@@ -21,12 +21,12 @@ var STEP_TIME = Math.floor(1000/30);
 var PLAYER_COMPLETE_MOVEMENT_MS = 1000;
 var ZERO_EPS = 0.1;
 
-var PLAYER_SPEED = 125;
-var GUARD_SPEED = 100;
+//var PLAYER_SPEED = 125;
+//var GUARD_SPEED = 100;
 
-var MINION_ANIM_SCALE = 0.1;
-var PLAYER_ANIM_SCALE = 0.34;
-var GUARD_ANIM_SCALE = 0.18;
+//var MINION_ANIM_SCALE = 0.1;
+//var PLAYER_ANIM_SCALE = 0.34;
+//var GUARD_ANIM_SCALE = 0.18;
 
 var restartingSprite = null;
 var restarting = false;
@@ -36,16 +36,16 @@ var restartingLengthMS = 2000;
 var scrollSprite = null;
 var showingScroll = false;
 
-var finalSprite1 = null;
-var finalSprite2 = null;
-var fadeSprite = null;
-var showingFinal = false;
+//var finalSprite1 = null;
+//var finalSprite2 = null;
+//var fadeSprite = null;
+//var showingFinal = false;
 var finalStartTimeMS = null;
-var blendAfterMS = 800;
-var blendForMS = 500;
-var fadeAfterMS = 10000;
-var reloadAfterMS = 15000;
-var MAX_HP = 1000;
-var health;
-var money;
-var command = null;
+//var blendAfterMS = 800;
+//var blendForMS = 500;
+//var fadeAfterMS = 10000;
+//var reloadAfterMS = 15000;
+//var MAX_HP = 1000;
+//var health;
+//var money;
+//var command = null;
--- client/js/levelLoader.js
+++ client/js/levelLoader.js
@@ -3,9 +3,9 @@ function loadLevel(num) {
 	localStorage.setItem('level', num);
 
 	var levelData = levels[num];
-	health = MAX_HP;
-	money = levelData['money'];
-	updateMoney(0);
+	// health = MAX_HP;
+	// money = levelData['money'];
+	// updateMoney(0);
 
 	if (!state) {
 		state = {};
@@ -16,17 +16,17 @@ function loadLevel(num) {
 	var map = levelData.map;
 	var definition = levelData.definition;
 	var currentLevel = state.currentLevel;
-	state.waves = [];
-	state.towers = [];
-	state.projectiles = [];
-	state.tower_templates = levelData.tower_templates;
-	state.buildable = new Array(map.length);
-	state.passable = new Array(map.length);
-	state.exit = new Array(map.length);
-	state.flow = new Array(map.length);
-	state.units = [];
-	state.unitCell = new Array(map.length);
-	var exits = [];
+	// state.waves = [];
+	// state.towers = [];
+	// state.projectiles = [];
+	// state.tower_templates = levelData.tower_templates;
+	// state.buildable = new Array(map.length);
+	// state.passable = new Array(map.length);
+	// state.exit = new Array(map.length);
+	// state.flow = new Array(map.length);
+	// state.units = [];
+	// state.unitCell = new Array(map.length);
+	// var exits = [];
 	var dist = new Array(map.length);
 	state.levelGridHeight = map.length;
 	for (var y = 0; y < map.length; y++) {
@@ -50,11 +50,11 @@ function loadLevel(num) {
 				var tile = $.extend(true, {} , definition[tileType]);
 				buildable[x] |= tile.buildable;
 				passable[x] &= tile.passable;
-				if (tile.type === "exit" ){
-					exit[x] = true;
-					exits.push([y, x]);
-					d[x] = 0;
-				}
+				// if (tile.type === "exit" ){
+				// 	exit[x] = true;
+				// 	exits.push([y, x]);
+				// 	d[x] = 0;
+				// }
 				currentLevel[y].push(tile);
 			}
 			else
@@ -65,20 +65,20 @@ function loadLevel(num) {
 					var tile = $.extend(true, {} , definition[tileType]);
 					buildable[x] |= tile.buildable;
 					passable[x] &= tile.passable;
-					if (tile.type === "exit" ){
-						exit[x] = true;
-						exits.push([y, x]);
-						d[x] = 0;
-					}
+					// if (tile.type === "exit" ){
+					// 	exit[x] = true;
+					// 	exits.push([y, x]);
+					// 	d[x] = 0;
+					// }
 					tiles.push(tile);
 				}
 				currentLevel[y].push(tiles);
 			}
 		};
-		state.buildable[y] = buildable;
-		state.passable[y] = passable;
-		state.exit[y] = exit;
-		state.flow[y] = flow;
+		// state.buildable[y] = buildable;
+		// state.passable[y] = passable;
+		// state.exit[y] = exit;
+		// state.flow[y] = flow;
 		state.unitCell[y] = cell;
 		dist[y] = d;
 	};
@@ -105,96 +105,96 @@ function loadLevel(num) {
 		}
 	}
 	// Flow calculations
-	for (var cy=0; cy<map.length; ++cy){
-		for (var cx=0; cx<map[cy].length; ++cx){
-			if (state.passable[cy][cx]){
-				var min_dist = dist[cy][cx];
-				var flow = state.flow[cy][cx]; 
-				for (var j=0; j<mods.length; ++j){
-					var mod = mods[j];
-					var y = cy + mod[0];
-					var x = cx + mod[1];
-					if ((0 <= y) && (y<map.length) && (0<=x) && (x<map[y].length) && (state.passable[y][x])){
-						if (dist[y][x] < min_dist){
-							min_dist = dist[y][x];
-							flow = [mod];
-						}
-						else if (dist[y][x] === min_dist){
-							flow.push(mod);
-						}
-					}
-				}
-				state.flow[cy][cx] = flow;
-			}
-		}
-	}
+	// for (var cy=0; cy<map.length; ++cy){
+	// 	for (var cx=0; cx<map[cy].length; ++cx){
+	// 		if (state.passable[cy][cx]){
+	// 			var min_dist = dist[cy][cx];
+	// 			var flow = state.flow[cy][cx];
+	// 			for (var j=0; j<mods.length; ++j){
+	// 				var mod = mods[j];
+	// 				var y = cy + mod[0];
+	// 				var x = cx + mod[1];
+	// 				if ((0 <= y) && (y<map.length) && (0<=x) && (x<map[y].length) && (state.passable[y][x])){
+	// 					if (dist[y][x] < min_dist){
+	// 						min_dist = dist[y][x];
+	// 						flow = [mod];
+	// 					}
+	// 					else if (dist[y][x] === min_dist){
+	// 						flow.push(mod);
+	// 					}
+	// 				}
+	// 			}
+	// 			state.flow[cy][cx] = flow;
+	// 		}
+	// 	}
+	// }
 	// Wave management
-	for (var i=0; i<levelData.waves.length; ++i){
-		var wave = $.extend(true, {} , levelData.waves[i]);
-		wave.started = false;
-		wave.passedMS = 0;
-		wave.lastSpawnMS = 0;
-		state.waves.push(wave);
-	}
+	// for (var i=0; i<levelData.waves.length; ++i){
+	// 	var wave = $.extend(true, {} , levelData.waves[i]);
+	// 	wave.started = false;
+	// 	wave.passedMS = 0;
+	// 	wave.lastSpawnMS = 0;
+	// 	state.waves.push(wave);
+	// }
 }
 
-var interactableUpdates = {
-	"spikes" : function(spikes, now) {
-		if (spikes.nextShow == undefined) {
-			spikes.nextShow = now + 2000 + (Math.random() * 1200);
-		}
+// var interactableUpdates = {
+// 	"spikes" : function(spikes, now) {
+// 		if (spikes.nextShow == undefined) {
+// 			spikes.nextShow = now + 2000 + (Math.random() * 1200);
+// 		}
 
-		if (now > spikes.nextShow && spikes.spriteHidden.visible) {
-			spikes.nextHide = now + 5000;
-			spikes.spriteShown.visible = true;
-			spikes.spriteHidden.visible = false;
-			spikesFX.currentTime = 0;
-			spikesFX.play();
-		} else if (now > spikes.nextHide && spikes.spriteShown.visible) {
-			spikes.nextShow = now + 2000;
-			spikes.spriteShown.visible = false;
-			spikes.spriteHidden.visible = true;
-		}
+// 		if (now > spikes.nextShow && spikes.spriteHidden.visible) {
+// 			spikes.nextHide = now + 5000;
+// 			spikes.spriteShown.visible = true;
+// 			spikes.spriteHidden.visible = false;
+// 			spikesFX.currentTime = 0;
+// 			spikesFX.play();
+// 		} else if (now > spikes.nextHide && spikes.spriteShown.visible) {
+// 			spikes.nextShow = now + 2000;
+// 			spikes.spriteShown.visible = false;
+// 			spikes.spriteHidden.visible = true;
+// 		}
 
-		if (spikes.spriteHidden.visible) {
-			return;
-		}
+// 		if (spikes.spriteHidden.visible) {
+// 			return;
+// 		}
 
-		var playerMapPos = realToMapPos( player.container.position );
-		var spikesMapPos = realToMapPos( spikes.spriteShown.position );
-		if (sqrDist(playerMapPos, spikesMapPos) < 1) {
-			playerDied(now);
-		}
-	},
+// 		var playerMapPos = realToMapPos( player.container.position );
+// 		var spikesMapPos = realToMapPos( spikes.spriteShown.position );
+// 		if (sqrDist(playerMapPos, spikesMapPos) < 1) {
+// 			playerDied(now);
+// 		}
+// 	},
 
-	"door" : function(door, x, y) {
-		if (!player.container) {
-			return;
-		}
-		var playerMapPos = realToMapPos( player.container.position );
-		var doorMapPos = realToMapPos( door.spriteOpenned.position );
-		if (sqrDist(playerMapPos, doorMapPos) < 1) {
-			if (door.final) {
-				showFinal();
-			} else {
-				goToNextLevel();
-			}
+// 	"door" : function(door, x, y) {
+// 		if (!player.container) {
+// 			return;
+// 		}
+// 		var playerMapPos = realToMapPos( player.container.position );
+// 		var doorMapPos = realToMapPos( door.spriteOpenned.position );
+// 		if (sqrDist(playerMapPos, doorMapPos) < 1) {
+// 			if (door.final) {
+// 				showFinal();
+// 			} else {
+// 				goToNextLevel();
+// 			}
 			
-		}
-	},
+// 		}
+// 	},
 
-	"scroll" : function(scroll, x , y) {
-		if (!player.container || !scroll.sprites.scroll.visible) {
-			return;
-		}
+// 	"scroll" : function(scroll, x , y) {
+// 		if (!player.container || !scroll.sprites.scroll.visible) {
+// 			return;
+// 		}
 
-		var playerMapPos = realToMapPos( player.container.position );
-		var doorMapPos = realToMapPos( scroll.sprites.scroll );
-		if (sqrDist(playerMapPos, doorMapPos) < 1) {
-			scroll.open();
-		}
-	}
-}
+// 		var playerMapPos = realToMapPos( player.container.position );
+// 		var doorMapPos = realToMapPos( scroll.sprites.scroll );
+// 		if (sqrDist(playerMapPos, doorMapPos) < 1) {
+// 			scroll.open();
+// 		}
+// 	}
+// }
 
 function updateLevel(now) {
 	var currentLevel = state.currentLevel;
